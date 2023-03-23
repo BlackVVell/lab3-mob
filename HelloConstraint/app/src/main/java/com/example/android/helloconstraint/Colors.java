c8d537e73b39e51ded67a411cdc2911076131c78
BlackVVell <tatarchuk2000@gmail.com>
2023-03-23 12:43:32 +0200

    Lab3 - task 1 (fix)

---
+++ HelloConstraint/app/src/main/java/com/example/android/helloconstraint/Colors.java
@@ -0,0 +1,12 @@
+package com.example.android.helloconstraint;
+
+import android.graphics.Color;
+
+public class Colors {
+    static int COLOR_PRIMARY = Color.parseColor("#3F51B5");
+    static int COLOR_PRIMARY_DARK = Color.parseColor("#303F9F");
+    static int COLOR_ACCENT = Color.parseColor("#FF4081");
+    static int COLOR_PURPLE = Color.parseColor("#E15BF8");
+    static int COLOR_GREEN = Color.parseColor("#51B53F");
+    static int COLOR_GREY = Color.parseColor("#959595");
+}
